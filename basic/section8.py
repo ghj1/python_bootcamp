dc84f43ec342333ae20a587d96662026416530e9
ghj1 <ghj12222@gmail.com>
2022-07-12 13:09:29 +0900

    cm

--- basic/section8.py
+++ basic/section8.py
@@ -7,8 +7,15 @@
 
 
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}")
     
-greet_with_name("Angela")
+# greet_with_name("Angela")
+
+# funtions with more than 1 input 
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+    
+greet_with(name = "Angela", location = "USA")
---
+++ basic/section8_1_excercise.py
@@ -0,0 +1,20 @@
+# excercise_8_1 페인트 면적 계산기 
+
+import math 
+def paint_calc(height, width, cover):
+    number_of_cans = (height * width) / cover
+    return number_of_cans
+    
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+result = math.ceil(paint_calc(height=test_h, width=test_w, cover=coverage))
+print(f"You'll need {result} cans of paint")
+
+# excercise_8_1 강사님 result
+import math 
+def paint_calc(height, width, cover):
+    area = height * width
+    num_of_cans = math.ceil(area / cover)
+    print(f"You'll need {num_of_cans} cans of paint")
