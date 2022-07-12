3498f6b5a10e8c1eaad6162ccb88b6df0a4a5dcf
ghj1 <ghj12222@gmail.com>
2022-07-12 12:17:08 +0900

    cm

---
+++ basic/section8.py
@@ -0,0 +1,14 @@
+# def greet():
+#     print("hello Angela")
+#     print("How do you do Jack Bauer?")
+#     print("Isn't the weather nice today")
+    
+# greet()
+
+
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+    
+greet_with_name("Angela")
